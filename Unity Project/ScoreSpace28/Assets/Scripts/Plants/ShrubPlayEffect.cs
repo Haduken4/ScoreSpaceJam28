91ccdaa87230b693c1ffe8aa0bccb4397ee87187
BarBenzvi <bar_bar@live.com>
2024-01-27 16:36:55 -0800

    Added shrub card

---
+++ Unity Project/ScoreSpace28/Assets/Scripts/Plants/ShrubPlayEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrubPlayEffect : OnPlayEffect
+{
+    protected override void GameplayEffect()
+    {
+        return; // Does nothing
+    }
+}
