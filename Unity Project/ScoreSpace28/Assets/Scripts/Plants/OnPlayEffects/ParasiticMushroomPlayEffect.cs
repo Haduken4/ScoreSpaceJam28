536b3a4b5a77463e68ce565f8aa61cae20531c3e
BarBenzvi <bar_bar@live.com>
2024-03-10 01:03:30 -0800

    started new parasitic mush

--- Unity Project/ScoreSpace28/Assets/Scripts/Plants/OnPlayEffects/ParasiticMushroomPlayEffect.cs
+++ Unity Project/ScoreSpace28/Assets/Scripts/Plants/OnPlayEffects/ParasiticMushroomPlayEffect.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ParasiticMushroomPlayEffect : OnPlayEffect
 {
     public GameObject BeePrefab = null;
+    public float ScoreGainsPerSiphon = 0.5f;
+    public float SizeGainsPerSiphon = 0.1f;
+
+    public List<PlantIdentifier> SiphonTargets = new List<PlantIdentifier>();
 
     protected override void GameplayEffect()
     {
