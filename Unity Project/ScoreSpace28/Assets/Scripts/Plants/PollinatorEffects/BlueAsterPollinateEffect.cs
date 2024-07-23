e35fff190f5f85f85de77baf6fe892a61892025a
BarBenzvi <bar_bar@live.com>
2024-07-22 17:29:27 -0700

    Added a particle effect for blue aster

--- Unity Project/ScoreSpace28/Assets/Scripts/Plants/PollinatorEffects/BlueAsterPollinateEffect.cs
+++ Unity Project/ScoreSpace28/Assets/Scripts/Plants/PollinatorEffects/BlueAsterPollinateEffect.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class BlueAsterPollinateEffect : OnPollinateEffect
 {
     public float ExtraScoreOnPollinate = 4;
+    public ParticleSystem PollinateParticles = null;
 
     public override void PollinateEffect()
     {
         pd.AddScore(Mathf.Ceil(ExtraScoreOnPollinate));
+        if (PollinateParticles)
+        {
+            PollinateParticles.Play();
+        }
     }
 }
